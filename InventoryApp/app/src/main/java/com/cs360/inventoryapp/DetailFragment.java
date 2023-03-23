50c5c65fc74841a9b42e94ccbb54944e00d518fb
William Brandow <>
2023-03-22 22:15:55 -0400

    refactoring

--- InventoryApp/app/src/main/java/com/cs360/inventoryapp/DetailFragment.java
+++ InventoryApp/app/src/main/java/com/cs360/inventoryapp/DetailFragment.java
@@ -15,9 +15,8 @@ public class DetailFragment extends Fragment {
     private Item mItem;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         int itemUid = 0;
 
         // Get the band ID from the fragment arguments
@@ -26,19 +25,8 @@ public class DetailFragment extends Fragment {
             itemUid = args.getInt(ITEM_UID);
         }
 
-        // FIXME: get mItem from database using UID
-
-        mItem = new Item("Detail Item", 1234, "Detail description", 99);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        TextView textView = (TextView) view.findViewById(R.id.detail_item_name);
-        textView.setText(mItem.getItemName());
 
         return view;
     }
