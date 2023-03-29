1e7e836b6d69ff906a799a6394dc74ef8071f300
William Brandow <william.a.brandow@gmail.com>
2023-03-29 17:20:40 -0400

    Refactoring DetailFragment

--- InventoryApp/app/src/main/java/com/cs360/inventoryapp/DetailFragment.java
+++ InventoryApp/app/src/main/java/com/cs360/inventoryapp/DetailFragment.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -25,27 +24,20 @@ public class DetailFragment extends Fragment {
     public static final String ITEM_UID = "uid";
     public static final String ITEM_DESCRIPTION = "description";
     public static final String ITEM_QUANTITY = "quantity";
-    private EditText mEditTextName;
-    private EditText mEditTextUid;
-    private EditText mEditTextDescription;
-    private EditText mEditTextQuantity;
-
-    private Button mButtonSave;
-    private Button mButtonDelete;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-        mButtonSave = view.findViewById(R.id.button_save);
-        mButtonDelete = view.findViewById(R.id.button_delete);
+        Button buttonSave = rootView.findViewById(R.id.button_save);
+        Button buttonDelete = rootView.findViewById(R.id.button_delete);
 
-        mEditTextName = view.findViewById(R.id.edittext_detail_item_name);
-        mEditTextUid = view.findViewById(R.id.edittext_detail_item_uid);
-        mEditTextDescription = view.findViewById(R.id.edittext_detail_item_description);
-        mEditTextQuantity = view.findViewById(R.id.edittext_detail_item_quantity);
+        EditText editTextName = rootView.findViewById(R.id.edittext_detail_item_name);
+        EditText editTextUid = rootView.findViewById(R.id.edittext_detail_item_uid);
+        EditText editTextDescription = rootView.findViewById(R.id.edittext_detail_item_description);
+        EditText editTextQuantity = rootView.findViewById(R.id.edittext_detail_item_quantity);
 
         // Get the clickedItem info from ListFragment
         Bundle args = getArguments();
@@ -55,123 +47,116 @@ public class DetailFragment extends Fragment {
             String itemDescription = args.getString(ITEM_DESCRIPTION, null);
             int itemQuantity = args.getInt(ITEM_QUANTITY, -9999);
 
-            mEditTextName.setText(itemName);
-            mEditTextUid.setText(Integer.toString(itemUid));
-            mEditTextDescription.setText(itemDescription);
-            mEditTextQuantity.setText(Integer.toString(itemQuantity));
+            editTextName.setText(itemName);
+            editTextUid.setText(Integer.toString(itemUid));
+            editTextDescription.setText(itemDescription);
+            editTextQuantity.setText(Integer.toString(itemQuantity));
         }
 
-        mButtonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String notification;
-                String name = String.valueOf(mEditTextName.getText());
-                String description = String.valueOf(mEditTextDescription.getText());
-                try {
-                    int uid = Integer.parseInt(String.valueOf(mEditTextUid.getText()));
-                    int quantity = Integer.parseInt(String.valueOf(mEditTextQuantity.getText()));
+        buttonSave.setOnClickListener(view -> {
+            String toastMessage;
+            String name = String.valueOf(editTextName.getText());
+            String description = String.valueOf(editTextDescription.getText());
+            try {
+                int uid = Integer.parseInt(String.valueOf(editTextUid.getText()));
+                int quantity = Integer.parseInt(String.valueOf(editTextQuantity.getText()));
 
-                    ItemService itemService = ItemService.getItemService();
-                    List<Item> items = itemService.getItemList();
+                ItemService itemService = ItemService.getItemService();
+                List<Item> items = itemService.getItemList();
 
-                    boolean updated = false;
-                    for(int i = 0; i < items.size(); i++) {
-                        if(uid == items.get(i).getItemUid()) {
-                            itemService.updateItem(name, uid, description, quantity);
-                            updated = true;
+                boolean updated = false;
+                for (int i = 0; i < items.size(); i++) {
+                    if (uid == items.get(i).getItemUid()) {
+                        itemService.updateItem(name, uid, description, quantity);
+                        updated = true;
 
-                            // notify user of successful update
-                            notification = name + " sucessfully updated!";
-                            Context context = getContext();
-                            int duration = Toast.LENGTH_SHORT;
-                            Toast toast = Toast.makeText(context, notification, duration);
-                            toast.show();
-
-                            // navigate back to list
-                            Navigation.findNavController(view).navigate(R.id.list_fragment);
-                        }
-                    }
-                    if(updated == false) {
-                        Item newItem = new Item(name, uid, description, quantity);
-                        itemService.addItem(newItem);
-
-                        // notify user of successful addition
-                        notification = name + " added to inventory!";
+                        // notify user of successful update
+                        toastMessage = name + " successfully updated!";
                         Context context = getContext();
                         int duration = Toast.LENGTH_SHORT;
-                        Toast toast = Toast.makeText(context, notification, duration);
+                        Toast toast = Toast.makeText(context, toastMessage, duration);
                         toast.show();
 
                         // navigate back to list
                         Navigation.findNavController(view).navigate(R.id.list_fragment);
                     }
                 }
-                catch (NumberFormatException error) {
-                    String message = "onClick: Value entered into UID or Quantity not an integer.";
-                    Log.e(TAG, message, error);
+                if (!updated) {
+                    Item newItem = new Item(name, uid, description, quantity);
+                    itemService.addItem(newItem);
 
-                    // Prompt user to fix error
-                    notification = "UID and Quantity must both be integer values.";
+                    // notify user of successful addition
+                    toastMessage = name + " added to inventory!";
                     Context context = getContext();
                     int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, notification, duration);
+                    Toast toast = Toast.makeText(context, toastMessage, duration);
                     toast.show();
+
+                    // navigate back to list
+                    Navigation.findNavController(view).navigate(R.id.list_fragment);
                 }
+            } catch (NumberFormatException error) {
+                String message = "onClick: Value entered into UID or Quantity not an integer.";
+                Log.e(TAG, message, error);
+
+                // Prompt user to fix error
+                toastMessage = "UID and Quantity must both be integer values.";
+                Context context = getContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, toastMessage, duration);
+                toast.show();
             }
         });
 
-        mButtonDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String notification;
+        buttonDelete.setOnClickListener(view -> {
+            String toastMessage;
 
-                try {
-                    int uid = parseInt(String.valueOf(mEditTextUid.getText()));
+            try {
+                int uid = parseInt(String.valueOf(editTextUid.getText()));
 
-                    ItemService itemService = ItemService.getItemService();
-                    List<Item> items = itemService.getItemList();
+                ItemService itemService = ItemService.getItemService();
+                List<Item> items = itemService.getItemList();
 
-                    boolean deleted = false;
-                    for(int i = 0; i < items.size(); i++) {
-                        if(uid == items.get(i).getItemUid()) {
-                            itemService.deleteItem(uid);
-                            deleted = true;
+                boolean deleted = false;
+                for(int i = 0; i < items.size(); i++) {
+                    if(uid == items.get(i).getItemUid()) {
+                        itemService.deleteItem(uid);
+                        deleted = true;
 
-                            // notify of successful delete
-                            notification = "Item with UID: " + String.valueOf(uid) + " deleted.";
-                            Context context = getContext();
-                            int duration = Toast.LENGTH_SHORT;
-                            Toast toast = Toast.makeText(context, notification, duration);
-                            toast.show();
-
-                            // navigate back to list
-                            Navigation.findNavController(view).navigate(R.id.list_fragment);
-                        }
-                    }
-                    if(deleted == false) {
-                        // notify user of no item exists
-                        notification = "Item with UID: " + String.valueOf(uid) + " not found.";
+                        // notify of successful delete
+                        toastMessage = "Item with UID: " + uid + " deleted.";
                         Context context = getContext();
                         int duration = Toast.LENGTH_SHORT;
-                        Toast toast = Toast.makeText(context, notification, duration);
+                        Toast toast = Toast.makeText(context, toastMessage, duration);
                         toast.show();
+
+                        // navigate back to list
+                        Navigation.findNavController(view).navigate(R.id.list_fragment);
                     }
                 }
-                catch (NumberFormatException error) {
-                    String message = "onClick: Value entered into UID or Quantity not an integer.";
-                    Log.e(TAG, message, error);
-
-                    // Prompt user to fix error
-                    notification = "UID must be an integer value.";
+                if(!deleted) {
+                    // notify user of no item exists
+                    toastMessage = "Item with UID: " + uid + " not found.";
                     Context context = getContext();
                     int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, notification, duration);
+                    Toast toast = Toast.makeText(context, toastMessage, duration);
                     toast.show();
                 }
             }
+            catch (NumberFormatException error) {
+                String message = "onClick: Value entered into UID or Quantity not an integer.";
+                Log.e(TAG, message, error);
+
+                // Prompt user to fix error
+                toastMessage = "UID must be an integer value.";
+                Context context = getContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, toastMessage, duration);
+                toast.show();
+            }
         });
 
-        return view;
+        return rootView;
     }
 
 }
