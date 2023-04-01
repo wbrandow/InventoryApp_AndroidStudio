deaae0638f06c260b36d07659016a3e15bcfb9fa
William Brandow <william.a.brandow@gmail.com>
2023-03-31 21:38:59 -0400

    Removed navigate up from appbar

--- InventoryApp/app/src/main/java/com/cs360/inventoryapp/MainActivity.java
+++ InventoryApp/app/src/main/java/com/cs360/inventoryapp/MainActivity.java
@@ -1,17 +1,9 @@
 package com.cs360.inventoryapp;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,19 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
-
-        if (navHostFragment != null) {
-            NavController navController = navHostFragment.getNavController();
-            AppBarConfiguration appBarConfig = new AppBarConfiguration.
-                    Builder(navController.getGraph()).build();
-            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
-        }
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 
     @Override
@@ -52,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.notifications) {
-
             Navigation.findNavController(findViewById(R.id.nav_host_fragment)).navigate(R.id.notification_dialog_fragment);
         }
         return true;
