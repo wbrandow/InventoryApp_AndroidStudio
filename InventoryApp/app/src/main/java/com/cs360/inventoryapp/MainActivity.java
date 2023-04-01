a5bf227129988fcb9c0fb244c419468a450b1ea4
William Brandow <william.a.brandow@gmail.com>
2023-03-31 21:20:44 -0400

    Added UI for notification settings

--- InventoryApp/app/src/main/java/com/cs360/inventoryapp/MainActivity.java
+++ InventoryApp/app/src/main/java/com/cs360/inventoryapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.cs360.inventoryapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Menu mMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +40,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        mMenu = menu;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.notifications) {
+
+            Navigation.findNavController(findViewById(R.id.nav_host_fragment)).navigate(R.id.notification_dialog_fragment);
+        }
+        return true;
     }
 
 }
